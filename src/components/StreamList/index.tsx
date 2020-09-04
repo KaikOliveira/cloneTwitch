ca9e38169176d0f091120e82913bad8469fe2a5c
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-09-04 08:09:07 -0300

    pagInicial

--- src/components/StreamList/index.tsx
+++ src/components/StreamList/index.tsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import streamThumbnail from '../../images/stream_thumbnail.jpg';
 
-import { Container } from './styles';
+import {
+  List,
+  StreamContainer,
+  StreamRow,
+  StreamThumbnail,
+  StreamColumn,
+  StreamHeader,
+  StreamAvatar,
+  StreamUsername,
+  StreamDescription,
+  StreamCategory,
+  TagRow,
+  TagView,
+  TagText,
+} from './styles';
+
 
 const StreamList: React.FC = () => {
+  const StreamItem = () => (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+      <StreamColumn>
+        <StreamRow>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numberOfLines={1}>KaikOficial</StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>
+            Front-end Development KaikOliveira.github.io
+          </StreamDescription>
+          <StreamDescription numberOfLines={1}>
+            Science & Technology
+          </StreamDescription>
+        </StreamRow>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portugues</TagText>
+          </TagView>
+          <TagView>
+            <TagText>Web Development</TagText>
+          </TagView>
+        </TagRow>
+      </StreamColumn>
+    </StreamContainer>
+  )
   return (
     <List>
-      
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
     </List>
   );
 };
